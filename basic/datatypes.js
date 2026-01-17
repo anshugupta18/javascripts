bfe1d02fcbb76f90e28760f976933af7f1087a9b
anshugupta18 <guptanshu30480@gmail.com>
2026-01-17 01:44:59 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,21 @@
+"use strisct";
+// alert ( 3 + 3)
+
+console.log( 3 + 3)
+console.log("Anshu")
+
+
+
+
+let name ="Anshu"
+let age = 19
+let isLoggedIn = false
+
+
+//  number => 2 to power 53
+//  bigint
+//  string =>""
+//  boolean => true/false
+// null => standalone value
+// undefined =>
+ //  symbol =>uniqe'
